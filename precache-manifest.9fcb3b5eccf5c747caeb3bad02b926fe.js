6aa9e00ead2d87b4a288e9fb136593407ff62485
Yashveer Talan <yashveertalan@gmail.com>
2020-02-15 06:41:48 +0530

    Final deploy

---
+++ precache-manifest.9fcb3b5eccf5c747caeb3bad02b926fe.js
@@ -0,0 +1,306 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbcbcdec23be0c1bfdc7ee52d346daa5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a1f3b61cc556d48fe7a",
+    "url": "/static/css/2.099a284e.chunk.css"
+  },
+  {
+    "revision": "13f9d1bbbe27786d41e8",
+    "url": "/static/css/main.9824f33d.chunk.css"
+  },
+  {
+    "revision": "6a1f3b61cc556d48fe7a",
+    "url": "/static/js/2.66066bf2.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.66066bf2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "13f9d1bbbe27786d41e8",
+    "url": "/static/js/main.9443d2e5.chunk.js"
+  },
+  {
+    "revision": "3b92bfd29b303ef6eebc",
+    "url": "/static/js/runtime-main.4c64575d.js"
+  },
+  {
+    "revision": "f6963a82eedb8c7f951bfdad85e01a09",
+    "url": "/static/media/abhinav.f6963a82.jpg"
+  },
+  {
+    "revision": "31a1b1377c7daceb8863e84d2aed432f",
+    "url": "/static/media/aditya.31a1b137.jpg"
+  },
+  {
+    "revision": "a250203d4394eaf2ad81984d17cf7d8b",
+    "url": "/static/media/akash.a250203d.jpg"
+  },
+  {
+    "revision": "c0e9e1921eca79dd5634dc6eb9aaa47d",
+    "url": "/static/media/anchi.c0e9e192.jpg"
+  },
+  {
+    "revision": "25590b087fac3cc7c463a481052c7fa3",
+    "url": "/static/media/anik.25590b08.jpg"
+  },
+  {
+    "revision": "be3a0123a0a4b539defee28e36fb6b90",
+    "url": "/static/media/anuj.be3a0123.jpg"
+  },
+  {
+    "revision": "2d6ec1d351ffc6964410453ffc6198ae",
+    "url": "/static/media/anujv.2d6ec1d3.jpg"
+  },
+  {
+    "revision": "fa5217a512cf184313842cd4ef8ca19e",
+    "url": "/static/media/anupriya.fa5217a5.jpg"
+  },
+  {
+    "revision": "ecf1ba97398e83dcad3a43f31a192222",
+    "url": "/static/media/anurag.ecf1ba97.jpg"
+  },
+  {
+    "revision": "0ab3789b1cbff9df7e683b0fd856fe51",
+    "url": "/static/media/arpit.0ab3789b.jpg"
+  },
+  {
+    "revision": "35a985b367fa62ca92539c08163df9a3",
+    "url": "/static/media/aryaman.35a985b3.jpg"
+  },
+  {
+    "revision": "ed841858fb3a89229d21c671932796ff",
+    "url": "/static/media/aryan.ed841858.jpg"
+  },
+  {
+    "revision": "06a82af6c40a2c6a15f0181b41d24b13",
+    "url": "/static/media/athletics-men.06a82af6.jpg"
+  },
+  {
+    "revision": "8b25020aaf509d135065b8438ef70e3d",
+    "url": "/static/media/athletics-women.8b25020a.jpg"
+  },
+  {
+    "revision": "9fd4545a376c2f76e2762a5984c0b947",
+    "url": "/static/media/ayush.9fd4545a.jpg"
+  },
+  {
+    "revision": "34ef9ba243467b2ad64ee4eb62b68a88",
+    "url": "/static/media/badminton.34ef9ba2.jpg"
+  },
+  {
+    "revision": "80ef85f0447cb30b8f89c2b47f079740",
+    "url": "/static/media/basketball.80ef85f0.jpeg"
+  },
+  {
+    "revision": "121ec9d5dad99f1c5855c90a67d281b9",
+    "url": "/static/media/blob-shape-green.121ec9d5.svg"
+  },
+  {
+    "revision": "94d6f109f9834cf5e28446b7e54f434b",
+    "url": "/static/media/blob-shape-skyblue.94d6f109.svg"
+  },
+  {
+    "revision": "bdd1a9bc6d45ba4814443090f702c0ae",
+    "url": "/static/media/blob-shape-yellow.bdd1a9bc.svg"
+  },
+  {
+    "revision": "a6fa4a9ee9f589e97fabbafc01a4d235",
+    "url": "/static/media/board.a6fa4a9e.png"
+  },
+  {
+    "revision": "2feeda27410177d2f3edd56c1cbbbc45",
+    "url": "/static/media/border-blue.2feeda27.png"
+  },
+  {
+    "revision": "d79d744c68de0129724fc84a70b483e4",
+    "url": "/static/media/border-green.d79d744c.png"
+  },
+  {
+    "revision": "89bdcfd98bbd2a3b8c6b76ceb023fdd4",
+    "url": "/static/media/border-pink.89bdcfd9.png"
+  },
+  {
+    "revision": "f8bd8906b161f99fcf3bfbbee810255a",
+    "url": "/static/media/border-skyblue.f8bd8906.svg"
+  },
+  {
+    "revision": "45a7681689964f23a96cdcc156d0dadf",
+    "url": "/static/media/chess.45a76816.jpeg"
+  },
+  {
+    "revision": "f0c44295901ae7a19b9a6f0e045850b8",
+    "url": "/static/media/cricket.f0c44295.webp"
+  },
+  {
+    "revision": "4644a6807c3d584db55226ba82e09415",
+    "url": "/static/media/dhruv.4644a680.jpg"
+  },
+  {
+    "revision": "8d166cfcb2baa603dfbcc3b21d22149d",
+    "url": "/static/media/divya.8d166cfc.jpg"
+  },
+  {
+    "revision": "c8a97709f2edbd3df77ebedee7dc6122",
+    "url": "/static/media/divyanshu.c8a97709.jpg"
+  },
+  {
+    "revision": "872bba46b5a66b69ccf39eb42a5e056f",
+    "url": "/static/media/football.872bba46.jpeg"
+  },
+  {
+    "revision": "d9e4e571f57dad10380b0bf0a8f64540",
+    "url": "/static/media/gallery1.d9e4e571.jpg"
+  },
+  {
+    "revision": "79d000235ec51ed7c90206b749c5ecc4",
+    "url": "/static/media/gallery2.79d00023.jpg"
+  },
+  {
+    "revision": "7e03bbf78fc33acfbc22f24738b60b8f",
+    "url": "/static/media/gallery3.7e03bbf7.jpg"
+  },
+  {
+    "revision": "59e34f9b095f54e34e3af767cb8a35d7",
+    "url": "/static/media/gallery4.59e34f9b.jpg"
+  },
+  {
+    "revision": "0118719c5d1c59005d41ae531d43caf7",
+    "url": "/static/media/gallery5.0118719c.jpg"
+  },
+  {
+    "revision": "8e6655b90ca4cb5eb1c527e6336a766f",
+    "url": "/static/media/gallery6.8e6655b9.jpg"
+  },
+  {
+    "revision": "e551ce9a1fc4e426d8078f11a5c5ac7b",
+    "url": "/static/media/gallery7.e551ce9a.jpg"
+  },
+  {
+    "revision": "b07c5991dc662eb1852d45fd3876aa95",
+    "url": "/static/media/gallery8.b07c5991.jpg"
+  },
+  {
+    "revision": "da5043c86a0868555821229c8031d0f6",
+    "url": "/static/media/gallery9.da5043c8.jpg"
+  },
+  {
+    "revision": "d7cfef36074aa8300684069ae2eb3e1d",
+    "url": "/static/media/gaurav.d7cfef36.jpg"
+  },
+  {
+    "revision": "cfefd1811702d8b57169c32dde87280f",
+    "url": "/static/media/green-yellow-shape.cfefd181.png"
+  },
+  {
+    "revision": "da1a1de0bc1bf700ebcd24cb1557293a",
+    "url": "/static/media/heart-icon.da1a1de0.png"
+  },
+  {
+    "revision": "a2eb646edfa1e3d82780b96a131d442c",
+    "url": "/static/media/kabaddi.a2eb646e.jpg"
+  },
+  {
+    "revision": "ff8168911bdf250dd595d5902c0e7fbb",
+    "url": "/static/media/kavish.ff816891.jpg"
+  },
+  {
+    "revision": "76abc79d746f978ae73c0988d0f069d3",
+    "url": "/static/media/mainlogo-black.76abc79d.png"
+  },
+  {
+    "revision": "35ceb2aa986c16a2d3fe838357271f59",
+    "url": "/static/media/mainlogo2.35ceb2aa.png"
+  },
+  {
+    "revision": "aff011519a30ae7c06ea70b35805d26d",
+    "url": "/static/media/navneet.aff01151.jpg"
+  },
+  {
+    "revision": "03100bd70444cde75065cf1ae2cee294",
+    "url": "/static/media/nikita.03100bd7.jpg"
+  },
+  {
+    "revision": "96f40337687c94a4db77d06e7726ad2e",
+    "url": "/static/media/purple-blue-shape.96f40337.png"
+  },
+  {
+    "revision": "5d3d2e62bd45e8bdffe85fae265608a3",
+    "url": "/static/media/rahul.5d3d2e62.jpg"
+  },
+  {
+    "revision": "95460bd8e03cd6b0d9bc53153b0e2812",
+    "url": "/static/media/red-orange-shape.95460bd8.png"
+  },
+  {
+    "revision": "261e5b94544b7b57c256d9e22a4b73f8",
+    "url": "/static/media/richa.261e5b94.jpg"
+  },
+  {
+    "revision": "47aefefbd83f01ba6dee66601b2c5bf1",
+    "url": "/static/media/rigved.47aefefb.jpg"
+  },
+  {
+    "revision": "ca02dcbf047f994012a786620f894523",
+    "url": "/static/media/rikki.ca02dcbf.jpg"
+  },
+  {
+    "revision": "130651874dc3b4ac5f29bb811ba754ab",
+    "url": "/static/media/saloni.13065187.jpg"
+  },
+  {
+    "revision": "4677f4458bcd4bc192c259879f107a6f",
+    "url": "/static/media/shashank.4677f445.jpg"
+  },
+  {
+    "revision": "52b52ecfe998ed566754eccfce16b494",
+    "url": "/static/media/shivam.52b52ecf.jpg"
+  },
+  {
+    "revision": "c6adc2c89785133920f86f6686bec502",
+    "url": "/static/media/shruti.c6adc2c8.jpg"
+  },
+  {
+    "revision": "f4fc422ae6819619e205ce25c79b9715",
+    "url": "/static/media/somendra.f4fc422a.jpg"
+  },
+  {
+    "revision": "00bac435e7bb4cf692361319e57e69ca",
+    "url": "/static/media/sudhanshu.00bac435.jpg"
+  },
+  {
+    "revision": "a98d4b65251e080e1b867e9032edc7c6",
+    "url": "/static/media/table-tennis.a98d4b65.jpg"
+  },
+  {
+    "revision": "9fa0fcaac883ba09de52264847ce22b6",
+    "url": "/static/media/teaser-video.9fa0fcaa.mp4"
+  },
+  {
+    "revision": "8c78e575a42533060686b2e90bdd48cf",
+    "url": "/static/media/tushar.8c78e575.jpg"
+  },
+  {
+    "revision": "7b524f1af0bc52907991e11ea1fe1694",
+    "url": "/static/media/utsav.7b524f1a.jpg"
+  },
+  {
+    "revision": "800bf4be6bc7ae2df9e89362d3dc3247",
+    "url": "/static/media/vikrant.800bf4be.jpg"
+  },
+  {
+    "revision": "345a50ea034181e90ca74d4618f38906",
+    "url": "/static/media/volleyball.345a50ea.png"
+  },
+  {
+    "revision": "1fd8b66af83dfd76ecce674272ea76bd",
+    "url": "/static/media/yashveer.1fd8b66a.jpg"
+  },
+  {
+    "revision": "4a14f94f2f74f4f0f63eb6c2f3376a84",
+    "url": "/static/media/yogi.4a14f94f.jpg"
+  }
+]);
